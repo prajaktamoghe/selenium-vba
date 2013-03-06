2d3e9d1b6e9d4f330dcee29db1df743b05d8810d
florentbr@gmail.com <florentbr@gmail.com@c7c002be-f1dc-ce9f-6f07-6550bc5f707e>
2013-03-06 09:38:25 +0000

    Old version for backup.

---
+++ trunk/remote/LanchServer.bat
@@ -0,0 +1,4 @@
+
+
+java -jar selenium-server2.jar -interactive
+IF NOT %ERRORLEVEL% == 0 pause
---
+++ trunk/remote/build.bat
@@ -0,0 +1,6 @@
+
+
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe" selenium-core.csproj
+pause
+Selenium.xls
+rem IF NOT %ERRORLEVEL% == 0 pause
