6d806363068e0e74339060a5fa019429fc987a46
florentbr@gmail.com <florentbr@gmail.com@c7c002be-f1dc-ce9f-6f07-6550bc5f707e>
2014-02-01 01:38:49 +0000

    Added the getData for a table

---
+++ trunk/wrapper/ITable.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace SeleniumWrapper {
+    /// <summary>Commands for the Table web element</summary>
+
+    [Description("Commands for the Table web element")]
+    [Guid("61208894-B0CE-46C5-915A-BE15C121F199")]
+    [ComVisible(true), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface Table {
+
+        [Description("Return an array filled with data from a table element")]
+        object[,] getData(int firstRowsToSkip = 0, int lastRowsToSkip = 0);
+    }
+
+}
