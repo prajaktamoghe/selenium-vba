86c68a19c943321438ccbc575b3c9017ff5d4f26
florentbr <florentbr@gmail.com>
2014-02-01 01:26:14 +0000

    Fixed Marshaling + Code refactoring Added getData for an element list

--- wrapper/WebElementCollection.cs
+++ wrapper/WebElementCollection.cs
@@ -1,35 +1,47 @@
 ﻿using System.Collections;
-using System.Runtime.InteropServices;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System;
 
 namespace SeleniumWrapper {
 
     [Guid("DE94EB33-C367-4870-8FBC-7C9763568492")]
-    [ComVisible(true), InterfaceType(ComInterfaceType.InterfaceIsDual)]
-    public interface WebElementCollectionCOM{
+    [ComVisible(true), InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface IWebElementCollection {
 
         IEnumerator GetEnumerator();
 
+        [Description("Number of web elements")]
         int Count { get; }
 
-        new WebElement this[int index] { get; }
+        [Description("Get the WebElement at the provided index (Zero-based)")]
+        WebElement this[int index] { get; }
 
+        [Description("Return and array containing the text for each web element")]
+        object[,] getData(int firstRowsToSkip = 0, int lastRowsToSkip = 0);
     }
 
     [Description("WebElementCollection")]
     [Guid("236BC66E-35F0-41B5-80D3-460D25F4D2B3")]
-    [ComVisible(true), ComDefaultInterface(typeof(WebElementCollectionCOM)), ClassInterface(ClassInterfaceType.None)]
-    public class WebElementCollection : ArrayList, WebElementCollectionCOM {
+    [ComVisible(true), ComDefaultInterface(typeof(IWebElementCollection)), ClassInterface(ClassInterfaceType.None)]
+    public class WebElementCollection : ArrayList, IWebElementCollection {
+
+        private readonly WebDriver _webDriver;
 
         public WebElementCollection(int capacity) : base(capacity) { }
 
-        public WebElementCollection(WebDriver webDriver, ReadOnlyCollection<OpenQA.Selenium.IWebElement> webElements)
+        public WebElementCollection(WebDriver webDriver, IList<OpenQA.Selenium.IWebElement> webElements)
             : base(webElements.Count) {
+            _webDriver = webDriver;
             foreach (var ele in webElements)
                 base.Add(new WebElement(webDriver, ele));
         }
 
+        /// <summary>Get the WebElement at the provided index</summary>
+        /// <param name="index">Zero based index</param>
+        /// <returns>WebElement</returns>
         public new WebElement this[int index] {
             get { return (WebElement)base[index]; }
         }
@@ -38,10 +50,26 @@ namespace SeleniumWrapper {
             base.Add(webelement);
         }
 
-        public void Add( WebDriver webDriver, ReadOnlyCollection<OpenQA.Selenium.IWebElement> webElements) {
+        public void Add(WebDriver webDriver, IList<OpenQA.Selenium.IWebElement> webElements) {
             var elements = base.Capacity += webElements.Count;
             foreach (var ele in webElements)
                 base.Add(new WebElement(webDriver, ele));
+        }
+
+
+        public object[,] getData(int firstRowsToSkip = 0, int lastRowsToSkip = 0) {
+            int size = this.Count;
+            int lastRow = size - lastRowsToSkip;
+            int firstRow = firstRowsToSkip + 1;
+            object[,] table = (object[,])Array.CreateInstance(typeof(object), new[] { size - firstRowsToSkip - lastRowsToSkip, 1 }, new[] { 1, 1 }); ;
+            int r = 1;
+            foreach (WebElement item in this) {
+                if (r < firstRow || r > lastRow) continue;
+                var value =  item._webElement.Text;
+                double number;
+                table[r++, 1] = double.TryParse(value, out number) ? (object)number : (object)value;
+            }
+            return table;
         }
 
     }
